039149e7efa0b9dc6d795b23413a0f956437741b
shalqarov <shalqarov01@gmail.com>
2023-02-16 14:41:36 +0600

    add: chi router and json encoder

--- service1/main.go
+++ service1/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"flag"
+	"log"
 	"math/rand"
+	"net/http"
 	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 var letterRunes = []rune("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+
+type Salt struct {
+	Salt string `json:"salt"`
+}
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
@@ -21,5 +31,25 @@ func Generate() string {
 }
 
 func main() {
-	fmt.Println(Generate())
+	port := flag.String("addr", ":8080", `Network Port
+	USAGE:
+	--addr=:8080`)
+	flag.Parse()
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/generate-salt", func(w http.ResponseWriter, r *http.Request) {
+		s := &Salt{Generate()}
+		w.Header().Set("Content-Type", "application/json")
+
+		if err := json.NewEncoder(w).Encode(s); err != nil {
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	})
+
+	log.Printf("generate-salt started at %s\n", *port)
+	if err := http.ListenAndServe(*port, r); err != nil {
+		log.Fatalln(err)
+	}
 }
